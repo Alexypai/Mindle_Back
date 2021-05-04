458a6e09cb0c27e4b906d282a7dda6f0c405f550
erichuytran <erictran@live.fr>
2021-05-04 15:01:45 +0200

    Initialisation Mindle Back

---
+++ src/main/java/IPI/mindle/mindleBack/MindleBackApplication.java
@@ -0,0 +1,13 @@
+package IPI.mindle.mindleBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindleBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindleBackApplication.class, args);
+	}
+
+}
