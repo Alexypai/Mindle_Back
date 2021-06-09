938649c38fc815b320392fb34495668ea14dd3a2
Alexy <alexy.paiva@ipilyon.net>
2021-06-09 22:44:42 +0200

    Update Présentation 14 Juin

--- src/main/java/com/ipiecoles/mindleBack/entity/sousGenres.java
+++ src/main/java/com/ipiecoles/mindleBack/entity/sousGenres.java
@@ -4,30 +4,30 @@ import java.util.List;
 
 public class sousGenres {
 
-    public String Name ;
-    public Integer Total;
-    public List<String> Genre;
+    public String name;
+    public Integer total;
+    public List<String> genre;
 
-    public String getName() { return Name; }
+    public String getName() { return name; }
 
     public void setName(String name) {
-        Name = name;
+        this.name = name;
     }
 
     public Integer getTotal() {
-        return Total;
+        return total;
     }
 
     public void setTotal(Integer total) {
-        Total = total;
+        this.total = total;
     }
 
     public List<String> getGenre() {
-        return Genre;
+        return genre;
     }
 
     public void setGenre(List<String> genre) {
-        Genre = genre;
+        this.genre = genre;
     }
 
 }
--- src/main/java/com/ipiecoles/mindleBack/ravenDB/manage/RavenManage.java
+++ src/main/java/com/ipiecoles/mindleBack/ravenDB/manage/RavenManage.java
@@ -7,7 +7,6 @@ import static com.ipiecoles.mindleBack.entity.listGenres.listMainGenres;
 import static com.ipiecoles.mindleBack.ravenDB.connexion.RavenCloudConnexion.session;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class RavenManage {
@@ -15,37 +14,36 @@ public class RavenManage {
     public static void AddDataUsers(listGenres Data, String Genres,Integer Choice){
         List<String> mainGenres = null;
         for (sousGenres sousGenre: Data.listGenres) {
-            if(sousGenre.Name.equals(Genres)){
+            if(sousGenre.name.equals(Genres)){
                 System.out.println("oui");
                 switch(Choice) {                // Provisoire
                     case 1:
-                        sousGenre.Total = sousGenre.Total + 1;
+                        sousGenre.total = sousGenre.total + 1;
                         break;
                     case 2:
-                        sousGenre.Total = sousGenre.Total - 1;
+                        sousGenre.total = sousGenre.total - 1;
                         break;
                     default:
                         continue;
                 }
-                mainGenres = sousGenre.Genre;
+                mainGenres = sousGenre.genre;
                 System.out.println(mainGenres);
             }
         }
         if (mainGenres.size() != 0){
             for (sousGenres sousGenre: Data.listGenres) {
                 for (String mainGenre : mainGenres){
-                    if (mainGenre.equals(sousGenre.Name)){
+                    if (mainGenre.equals(sousGenre.name)){
                         switch(Choice) {                        // Provisoire
                             case 1:
-                                sousGenre.Total = sousGenre.Total + 1;
+                                sousGenre.total = sousGenre.total + 1;
                                 break;
                             case 2:
-                                sousGenre.Total = sousGenre.Total - 1;
+                                sousGenre.total = sousGenre.total - 1;
                                 break;
                             default:
                                 continue;
                         }
-                        sousGenre.Total = sousGenre.Total + 1;
                     }
                 }
             }
@@ -59,21 +57,26 @@ public class RavenManage {
         Integer SizeDemo = 0;
         for (String MainGenre : listMainGenres) {
             for (sousGenres sousGenre: Data.listGenres) {
-                if (sousGenre.Name.equals(MainGenre)){
+                if (sousGenre.name.equals(MainGenre)){
                     ///////////////PROVISOIRE//////////////////
-                    if (sousGenre.Total >= SizeDemo) { MainGenreDemo = sousGenre.Name; }
+                    if (sousGenre.total >= SizeDemo) {
+                        MainGenreDemo = sousGenre.name;
+                        SizeDemo = sousGenre.total;
+                    }
                     ///////////////PROVISOIRE//////////////////
                     ArrayList<String> Provisoire = new ArrayList();
-                    Provisoire.add(sousGenre.Name);
-                    Provisoire.add(sousGenre.Total.toString());
+                    Provisoire.add(sousGenre.name);
+                    Provisoire.add(sousGenre.total.toString());
                     Totals.add(Provisoire);
                 }
             }
         }
-        if (MainGenreDemo.equals("")){
+        if (MainGenreDemo.equals("") || SizeDemo.equals(0)){
             MainGenreDemo =  "World-Music";
         }
         System.out.println(MainGenreDemo);
+        System.out.println(Totals);
+
         return MainGenreDemo;
     }
 
